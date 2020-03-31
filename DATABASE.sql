8bf6063a947ff0d097382f3d87fe0835659f6376
KhajaAmeenuddin1 <reinbreaker57@gmail.com>
2020-03-30 19:59:53 -0500

    creates db and tables

---
+++ DATABASE.sql
@@ -0,0 +1,42 @@
+drop DATABASE if exists Dietation;
+CREATE DATABASE Dietation;
+
+use Dietation;
+
+
+create table if not exists Users (
+	UserID int not null auto_increment,
+    FirstName varchar(255) not null,
+    LastName varchar(255) not null,
+    Primary key (UserID)
+);
+
+
+create table if not exists FoodFilter (
+	FoodID int not null auto_increment,
+    FoodName varchar(255) not null,
+    GlutenFree int not null,
+    DairyFree int not null,
+    NutFree int not null,
+    Vegan int not null,
+    Pescatarian int not null,
+    Primary key (FoodID)
+);
+    
+
+create table if not exists UserHistory (
+	OrderID int not null auto_increment,
+	UserID int not null,
+    FoodID int not null,
+    
+    
+    Primary key (OrderID),
+    foreign key(UserID)
+		references Users(UserID),
+	foreign key(FoodID)
+		references Foodfilter(FoodID)
+);
+    
+
+
+
